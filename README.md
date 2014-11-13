5a25043ad7095391e05ba7171f614f8e4c0c7f87
Dan Ford <dpford@gmail.com>
2014-11-13 09:14:59 -0500

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 timesheet-bot
 =============
+
+This bot will check to see if you've submitted hours for the previous day in your Unanet timesheet.  If not, it will add the number of hours of your choosing to your project, and save it.
+
+### Required software
+* [PhantomJS](http://phantomjs.org/download.html)
+
+### Required environment variables
+* `UNANET_URL` - the main Unanet login URL, e.g. https://www7.unanet.biz/company-name/action/home
+* `UNANET_USERNAME`
+* `UNANET_PASSWORD`
+* `UNANET_PROJECT` - the exact text inside the Project dropdown in your timesheet
+* `UNANET_HOURS` - the number of hours you want entered
+
+### Setup
+
+1. Clone the repo on your server
+2. Setup a virtual environment and install requirements.txt
+3. Add the environment variables to a sourceable file
+4. Customize job.sh if needed
+5. `crontab -e` and add something like:
+```
+30 9 * * 2-6 /path/to/job.sh
+```
+That will run the script at 9:30am local time Tues/Weds/Thurs/Fri/Sat.
--- timesheet.py
+++ timesheet.py
@@ -8,6 +8,7 @@ UNANET_URL = os.environ.get('UNANET_URL')
 UNANET_USERNAME = os.environ.get('UNANET_USERNAME')
 UNANET_PASSWORD = os.environ.get('UNANET_PASSWORD')
 UNANET_PROJECT = os.environ.get('UNANET_PROJECT')
+UNANET_HOURS = os.environ.get('UNANET_HOURS')
 
 # Login
 driver.get(UNANET_URL)
@@ -20,23 +21,28 @@ login.click()
 
 # Edit the active timesheet
 # TODO: Add case where there's no active timesheet
-edit_link = driver.find_element_by_xpath('//a[starts-with(@title, "Edit timesheet for")]')
+edit_link = driver.find_element_by_xpath(
+    '//a[starts-with(@title, "Edit timesheet for")]')
 edit_link.click()
 
-# Check to see if any hours were entered; if not, enter '8' for your project and save
+# Checks to see if any hours were entered for the day
+# If not, enter hours for your project and save
 day = datetime.date.today().day - 1
-position = driver.find_element_by_xpath('//span[text()={}]/..'.format(str(day))).get_attribute('id').split('_')[1]
+position = driver.find_element_by_xpath(
+    '//span[text()={}]/..'.format(str(day))).get_attribute('id').split('_')[1]
 if driver.find_element_by_name('t__{}'.format(position)).get_attribute('value'):
     with open('timesheets.log', 'a+') as f:
-        f.write('{}: You already entered hours for {} on {}!\n'.format(datetime.datetime.now(),
-                                                                       UNANET_PROJECT,
-                                                                       datetime.date.today() - datetime.timedelta(days=1)))
+        f.write('{}: You already entered hours for {} on {}!\n'.format(
+            datetime.datetime.now(),
+            UNANET_PROJECT,
+            datetime.date.today() - datetime.timedelta(days=1)))
 row = driver.find_element_by_xpath('//tr[@title={}]'.format(UNANET_PROJECT))
 row_id = row.get_attribute('id')
 cell = row.find_element_by_name('d_{}_{}'.format(row_id, position))
-cell.send_keys('8')
+cell.send_keys(UNANET_HOURS)
 driver.find_element_by_xpath('//button[@title="Save"]').click()
 with open('timesheets.log', 'a+') as f:
-    f.write('{}: Added 8 hours to {} for {}\n'.format(datetime.datetime.now(),
-                                                      UNANET_PROJECT,
-                                                      datetime.date.today() - datetime.timedelta(days=1)))
+    f.write('{}: Added 8 hours to {} for {}\n'.format(
+        datetime.datetime.now(),
+        UNANET_PROJECT,
+        datetime.date.today() - datetime.timedelta(days=1)))
